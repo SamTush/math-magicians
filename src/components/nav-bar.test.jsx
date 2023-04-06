8f50094e47b364fea56d568958740b32eb93a3ad
Rana-Mudassir <mduasire38@gmail.com>
2023-04-07 00:33:47 +0500

    Added tests to navbar

--- src/components/nav-bar.test.jsx
+++ src/components/nav-bar.test.jsx
@@ -1,1 +1,15 @@
-
+import { render, fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './nav-bar';
+describe('NavBar', () => {
+  test('navigates to the correct page when link is clicked', () => {
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>,
+    );
+    const calculatorLink = screen.getByText('Calculator');
+    fireEvent.click(calculatorLink);
+    expect(window.location.pathname).toBe('/calculator');
+  });
+});
